0c596a27e68a5f231800fe9d24665a5b2808ef8d
kite <kitefree@gmail.com>
2022-04-12 09:16:17 +0800

    back end update db config

--- law_be/db_con.php
+++ law_be/db_con.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'tqe');
 define('DB_PASSWORD', '0000');
-define('DB_NAME', 'law_test');
+define('DB_NAME', 'law');
 ?>
